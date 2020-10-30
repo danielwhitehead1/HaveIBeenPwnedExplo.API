db353cb4d4d3228489e27d925ad8ec9015ae8599
Whitehead <daniel.whitehead@sage.com>
2020-10-30 15:28:01 +0000

    Initial Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using HaveIBeenPwnedAPI.Models;
+using HaveIBeenPwnedAPI.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace HaveIBeenPwnedAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration config;
+
+        public LoginController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpPost]
+        [EnableCors("LoginPolicy")]
+        public async Task<ActionResult> Login([FromBody] User user)
+        {
+            string hashed = new HashingService().SHA1UTF8Hash(user.password);
+            string suffix = hashed.Substring(5);
+
+            HaveIBeenPwnedService service = new HaveIBeenPwnedService(config);
+            string breachedHashes = await service.GetBreachedHashes(hashed);
+
+            Regex reg = new Regex($@"{suffix}:([0-9]*)", RegexOptions.Singleline);
+
+            bool existing = reg.IsMatch(breachedHashes);
+            int occurances = Int32.Parse(reg.Matches(breachedHashes)[0].Groups[1].Value);
+
+            return Ok(new { breachedPassword = existing, numberOfBreaches =  occurances });
+        }
+    }
+}
---
+++ Services/HaveIBeenPwnedService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HaveIBeenPwnedAPI.Services
+{
+    public class HaveIBeenPwnedService
+    {
+        static readonly HttpClient client = new HttpClient();
+        private IConfiguration config;
+
+        public HaveIBeenPwnedService(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        public async Task<string> GetBreachedHashes(string hashedPassword)
+        {
+            string firstFiveChars = hashedPassword.Substring(0, 5);
+
+            HttpResponseMessage response = await client.GetAsync($"{config.GetSection("HaveIBeenPwnedAPIURL").Value}/{firstFiveChars}");
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            return responseContent;
+        }
+    }
+}
